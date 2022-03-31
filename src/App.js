5e80bc7434abc41a9184c9e6c9a9b071ecb2cb6e
Bipin Bhandari <bhandaribipin3.39@gmail.com>
2022-03-31 19:52:34 +0545

    deployed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
